c3b1a6b9256964595f86b120df9d83b5ed00220b
Fernando Molina <fmolina.it@gmail.com>
2021-10-04 17:47:25 -0300

    My config

--- rules.mk
+++ rules.mk
@@ -8,13 +8,14 @@ AUDIO_ENABLE = no           # Audio output on port C6
 MOUSEKEY_ENABLE = no
 BLUETOOTH_ENABLE  = no  # Enable Bluetooth with the Adafruit EZ-Key HID
 NKRO_ENABLE = no
+RGBLIGHT_ENABLE = no
+EXTRAKEY_ENABLE= no # For Media Keys
 
 # Yes ..
-OLED_DRIVER_ENABLE = yes
-EXTRAKEY_ENABLE= yes # For Media Keys
-RGBLIGHT_ENABLE = yes
+OLED_ENABLE = yes
 WPM_ENABLE = yes
 LTO_ENABLE = yes     # Makes hex file smaller
 
 # Elite C ..
 BOOTLOADER = qmk-dfu
+EXTRAFLAGS += -flto
